ec81994b29bc3ab3ebb3c6e98c34d8699361797c
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-02-09 01:35:09 +0500

    updating host

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_specific_keyphrase(keyphrase_id):
     return jsonify({"body":Controller2().get_specific_keyphrase(db, keyphrase_id)})
 
 if __name__ == "__main__":
-  app.run(host='127.0.0.1', port='8080', debug=False)
+  app.run(host='0.0.0.0', port='8080', debug=False)
