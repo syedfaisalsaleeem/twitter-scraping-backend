fc2fe755736ffaa70dbd0a721fea5258ce34cacb
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-02-16 00:25:18 +0500

    update to store csv only one time

--- controller.py
+++ controller.py
@@ -14,36 +14,18 @@ class Controller():
         key_phrases = self.key_phrases
         start_date = self.start_date
         end_date = self.end_date
-        limit = 1000
-        
         
         # Get the tweets
         tweets_list2 = []
         for key_phrase in key_phrases:
-            count = 0
-            data_inserted = False 
             store = StoreData(start_date, end_date, method=self.method, keyword=key_phrase)
             # Using TwitterSearchScraper to scrape data and append tweets to list
             for i, tweet in enumerate(sntwitter.
                                     TwitterSearchScraper(f'{key_phrase} since:{start_date} until:{end_date}').get_items()):
-                if count > limit:
-                    print('Updating Data')
-                    count = 0
-                    data_inserted = True
-                    try:
-                        # Insert data into csv file
-                        store.store_csv(tweets_list2)
-                        print(f'Latest Stored Tweet Date: {tweet.date}')
-                        store.store_csv_s3()
-                        print('files upload in s3')
-                    except Exception as e:
-                        print(e)
-                        print('Error in storing data')
-                    if self.breaking == True:
+                if self.breaking == True:
                         break
 
                 print(f'{len(tweets_list2)} tweets scrapped for "{key_phrase}". Tweet Date: {tweet.date}')
-                count += 1
                 values = [tweet.url, tweet.date, tweet.content, tweet.renderedContent, tweet.id, tweet.user.username,
                              tweet.user.id, tweet.user.displayname, tweet.user.description, tweet.user.rawDescription,
                              tweet.user.descriptionUrls, tweet.user.verified, tweet.user.created,
@@ -57,13 +39,15 @@ class Controller():
                              tweet.mentionedUsers, tweet.coordinates, tweet.place, tweet.hashtags, tweet.cashtags]
                 tweets_list2.append(values)
 
-            if data_inserted == False:
+            try:
                 # Insert data into csv file
                 store.store_csv(tweets_list2)
                 print(f'Latest Stored Tweet Date: {tweet.date}')
-                # Upload csv file to S3
                 store.store_csv_s3()
                 print('files upload in s3')
+            except Exception as e:
+                print(e)
+                print('Error in storing data')
 
 
 
