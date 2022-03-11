bf37b7198f9d13ebb3ca40e0175ac159500a490a
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-03-11 20:37:54 +0500

    simplifying deployment without docker

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='8080', debug=True)
+  app.run(host='0.0.0.0', port='80', debug=True)
